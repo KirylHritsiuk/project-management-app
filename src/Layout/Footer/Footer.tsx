3d5a66e9c8a2b9980c6d4420102ecc3e8ace4878
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-11-09 19:50:13 +0100

    refactor: updated footer

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,24 +1,35 @@
 import styles from './Footer.module.scss';
-import { ReactComponent as RssLogo } from './rssLogo.svg';
-import { ReactComponent as GitLogo } from './gitLogo.svg';
+import { ReactComponent as RssLogo } from './assets/rssLogo.svg';
+import { ReactComponent as GitLogo } from './assets/gitLogo.svg';
+import { ReactComponent as Copy } from './assets/copy.svg';
+
+const TEAMS = [
+  { login: 'KirylHritsiuk', link: 'https://github.com/KirylHritsiuk' },
+  { login: 'Philonena', link: 'https://github.com/Philonena' },
+  { login: 'kate-js', link: 'https://github.com/kate-js' },
+];
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <RssLogo className={styles.rss_logo} />
-        <p>2022</p>
+        <a href="https://rs.school/react/">
+          <RssLogo className={styles.rss_logo} />
+        </a>
         <ul className={styles.list}>
-          <li>
-            <GitLogo />
-          </li>
-          <li>
-            <GitLogo />
-          </li>
-          <li>
-            <GitLogo />
-          </li>
+          {TEAMS.map((item, index) => (
+            <a href={item.link} key={index} className={styles.list_link}>
+              <li>
+                <GitLogo className={styles.git_hub} />
+                <p>{item.login}</p>
+              </li>
+            </a>
+          ))}
         </ul>
+        <div className={styles.copy}>
+          <Copy className={styles.copy_icon} />
+          <p>2022</p>
+        </div>
       </div>
     </footer>
   );
