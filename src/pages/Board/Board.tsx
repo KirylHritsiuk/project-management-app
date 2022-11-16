e9ba880ed4bfe6f4e770e1c5eb4f80eb12ff320e
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-11-16 16:40:20 +0100

    refactor: updated Board page

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -7,9 +7,9 @@ import { Modal } from '../../components/UI/Modal/Modal';
 import { usePageNavigate } from '../../Hooks/usePageNavigate';
 import { TaskList } from '../../components/TaskList/TaskList';
 
-import { Button, LinearProgress } from '@mui/material';
+import { Button, LinearProgress, Stack } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-import Image from './delete_icon.png';
 import './Board.scss';
 
 export const Board = () => {
@@ -18,6 +18,8 @@ export const Board = () => {
   const iddd = id ?? '1';
   const { data, isLoading, error, refetch } = columnsAPI.useGetColumnsQuery({ boardId: iddd });
   const [isVisible, setVisible] = useState<boolean>(false);
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [delId, setDelId] = useState('');
   const [addColumn] = columnsAPI.useCreateColumnMutation();
   const [deleteColumn] = columnsAPI.useDeleteColumnMutation();
   const order: number = data?.length ? data?.length + 1 : 0;
@@ -33,6 +35,11 @@ export const Board = () => {
     setVisible(!isVisible);
   };
 
+  const changeOpen = (id: string) => {
+    setOpen(true);
+    setDelId(id);
+  };
+
   const onSubmit = (value: FieldValues) => {
     addColumn({ boardId: iddd, body: { title: value.title, order: order } })
       .then(() => {
@@ -45,10 +52,11 @@ export const Board = () => {
       });
   };
 
-  const delColumn = (id: string) => {
-    deleteColumn({ boardId: iddd, columnId: id })
+  const delColumn = async (id: string) => {
+    await deleteColumn({ boardId: iddd, columnId: id })
       .then(() => {
         refetch();
+        setOpen(false);
       })
       .catch((error) => {
         console.log(error);
@@ -63,34 +71,39 @@ export const Board = () => {
       <h2>Columns {id}</h2>
       {error && <span>error</span>}
       {isLoading && <LinearProgress />}
-      <ul className="list_column">
+      <Stack spacing={2} direction="row" alignItems="flex-start" justifyContent="flex-start">
         {data?.map((item, index) => (
-          <li key={index} className="card_column">
-            <div>
+          <div key={index} className="card_column">
+            <div className="card_title">
               <h3 className="column_title">{item.title}</h3>
-              <button onClick={() => delColumn(item._id)}>
-                <img src={Image} alt="Кнопка «button»" className="del_button" />
-              </button>
+              <DeleteForeverIcon
+                fontSize="large"
+                onClick={() => changeOpen(item._id)}
+              ></DeleteForeverIcon>
             </div>
             <TaskList boardId={iddd} columnId={item._id} />
-          </li>
+          </div>
         ))}
-        <button onClick={changeVisible} className="add_button">
+        <Button onClick={changeVisible} variant="outlined" color="success">
           +Add column
-        </button>
-      </ul>
-      <Modal visible={isVisible} setModal={setVisible}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="Title Column"
-            {...register('title', { required: true })}
-            aria-invalid={errors.title ? 'true' : 'false'}
-          />
-          {errors.title && <p role="alert">Please, input title</p>}
-          <input type="submit" />
-        </form>
-      </Modal>
+        </Button>
+        <Modal visible={isVisible} setModal={setVisible}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="Title Column"
+              {...register('title', { required: true })}
+              aria-invalid={errors.title ? 'true' : 'false'}
+            />
+            {errors.title && <p role="alert">Please, input title</p>}
+            <input type="submit" />
+          </form>
+        </Modal>
+        <Modal visible={isOpen} setModal={setOpen}>
+          <p>Вы действительно хотите удалить колонку?</p>
+          <Button onClick={() => delColumn(delId)}>Delete</Button>
+        </Modal>
+      </Stack>
     </div>
   );
 };
