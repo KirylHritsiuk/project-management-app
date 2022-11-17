7905f8127aab735876b307ad789d81521be92a47
KirylHritsiuk <pishime2017@gmail.com>
2022-11-17 14:05:00 +0300

    refactor: formatting boardlist

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,19 +1,40 @@
 import { LinearProgress } from '@mui/material';
-import { boardsAPI } from 'api/boardsApi';
-import { Board } from 'components';
+import { usersAPI } from 'api/usersApi';
+import BoardList from 'components/BoardList/BoardList';
+import { useAppSelector } from 'hooks/hooks';
+import { lazy, Suspense, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Navigate } from 'react-router-dom';
 import styled from './Main.module.scss';
 
+// const List = lazy(() => import('../../components/BoardList/BoardList'));
+
 export function Main() {
-  const { data, isLoading, error } = boardsAPI.useGetBoardsQuery('');
-  return (
+  const { isAuth, id } = useAppSelector((state) => state.user);
+  const { data: usersData } = usersAPI.useGetUsersQuery('');
+  const [name, setName] = useState<string>();
+
+  const { register, handleSubmit } = useForm<{ user: string }>({ defaultValues: { user: name } });
+  const onSubmit = (data: { user: string }) => console.log(data);
+
+  return isAuth ? (
     <main className={styled.main}>
-      {isLoading && <LinearProgress />}
-      {error && <span>error</span>}
-      <div className={styled.list}>
-        {data?.map((b) => (
-          <Board key={b._id} data={b} />
-        ))}
-      </div>
+      {/* <Suspense fallback={<LinearProgress />}> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <select {...register('user')}>
+          <option value={undefined}>All</option>
+          {usersData?.map((u) => (
+            <option key={u._id} value={u._id}>
+              {u.name}
+            </option>
+          ))}
+        </select>
+        <input type="submit" />
+      </form>
+      <BoardList />
+      {/* </Suspense> */}
     </main>
+  ) : (
+    <Navigate to={'/'} />
   );
 }
