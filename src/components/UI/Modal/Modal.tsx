b01c2caafa743837cf1f3e802848d5b1a2a122ec
KirylHritsiuk <pishime2017@gmail.com>
2022-11-24 11:39:58 +0300

    refactor: update modal animation

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -7,8 +7,12 @@ import { motion } from 'framer-motion';
 import { useMount } from 'hooks/useMount';
 
 const variants = {
-  open: { opacity: 1 },
+  open: { opacity: 1, scale: 1 },
   closed: { opacity: 0, scale: 0 },
+  init: { opacity: 0, scale: 0 },
+  openOverlay: { opacity: 1 },
+  closedOverlay: { opacity: 0 },
+  initOverlay: { opacity: 0 },
 };
 
 export const Modal: FC<ModalProps> = (props) => {
@@ -18,15 +22,25 @@ export const Modal: FC<ModalProps> = (props) => {
   if (mounted) {
     return (
       <Portal>
-        <motion.div
-          className={styles.modal}
-          animate={visible ? 'open' : 'closed'}
-          transition={{ duration: 0.3 }}
-          variants={variants}
-        >
-          <div className={styles.overlay} onClick={() => setModal(false)} />
-          <div className={cn(styles.content, className)}>{children}</div>
-        </motion.div>
+        <div className={styles.modal}>
+          <motion.div
+            className={styles.overlay}
+            onClick={() => setModal(false)}
+            initial="initOverlay"
+            animate={visible ? 'openOverlay' : 'closedOverlay'}
+            transition={{ duration: 0.3 }}
+            variants={variants}
+          />
+          <motion.div
+            className={cn(styles.content, className)}
+            initial="init"
+            animate={visible ? 'open' : 'closed'}
+            transition={{ duration: 0.3 }}
+            variants={variants}
+          >
+            {children}
+          </motion.div>
+        </div>
       </Portal>
     );
   }
